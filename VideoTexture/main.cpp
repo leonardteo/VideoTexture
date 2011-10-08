29ef7f8f8267737de085b4fdcd7da57532167109
Leonard Teo <teo.leonard@gmail.com>
2011-10-08 14:41:58 -0400

    Initial Commit

---
+++ VideoTexture/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  VideoTexture
+//
+//  Created by Leonard Teo on 11-10-08.
+//  Copyright 2011 Leonard Teo. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
